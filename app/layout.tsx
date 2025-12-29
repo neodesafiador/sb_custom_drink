4e6c8c5abab04b0ef2b81fcceb80a8fbfa27c6e0
Katsuki Oike <katsuki.oike@gmail.com>
2025-12-29 16:03:43 +0900

    Update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "カスタムドリンクメニュー",
-  description: "スターバックス風のカスタムドリンクメニューアプリ",
+  description: "スターバックスのカスタムドリンクメニューアプリ",
 };
 
 export default function RootLayout({
